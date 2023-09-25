0b0de005b531e93632134b8ef90af2f1a07af1ce
Eneias Morais <eneiasmorais@gmail.com>
2023-09-25 18:08:27 -0300

    fix: correção link LinkedIn

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -45,7 +45,13 @@ export const Contacts = () => {
                 <FaWhatsapp color="#fff" size={24} />
               </ContactCardImage>
               <ContactCardContent>
-                <Text type="heading4" color="grey4">
+                <Text
+                  as="a"
+                  type="heading4"
+                  color="grey4"
+                  target="_blank"
+                  href={`https://api.whatsapp.com/send?phone=+55+${userData.whatsappNumber}&text=Ol%C3%A1%2C%20venho%20por%20meio%20do%20seu%20portf%C3%B3lio%20na%20internet%2C%20gostaria%20de%20conhecer%20melhor%20seus%20servi%C3%A7os`}
+                >
                   My Whatsapp
                 </Text>
                 <Text color="grey2" type="body2">
@@ -69,7 +75,16 @@ export const Contacts = () => {
                 <FaEnvelopeOpen color="#fff" size={24} />
               </ContactCardImage>
               <ContactCardContent>
-                <Text type="heading4" color="grey4">
+                <Text
+                  as="a"
+                  type="heading4"
+                  color="grey4"
+                  target="_blank"
+                  href={`mailto=${userData.emailUser}`}
+                  onClick={() =>
+                    (window.location.href = "mailto:eneiasmorais@gmail.com")
+                  }
+                >
                   My email
                 </Text>
                 <Text color="grey2" type="body2">
@@ -95,7 +110,13 @@ export const Contacts = () => {
                 <FaLinkedin color="#fff" size={24} />
               </ContactCardImage>
               <ContactCardContent>
-                <Text type="heading4" color="grey4">
+                <Text
+                  as="a"
+                  type="heading4"
+                  color="grey4"
+                  target="_blank"
+                  href={linkedInUrl}
+                >
                   My LinkedIn
                 </Text>
                 <Text color="grey2" type="body2">
